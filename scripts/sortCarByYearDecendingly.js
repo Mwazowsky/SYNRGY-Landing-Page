1ca231da8b1ec178da46fb79bba76168e3e4a056
Saifulloh Fadli <saifulloh.fadli@gmail.com>
2023-10-10 15:26:20 +0700

    first commit

---
+++ scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,28 @@
+const sortArrayOfObjectDescending = (arr, propertyName) => {
+  const sortedArr = arr.sort((a, b) => {
+    if (a[propertyName] > b[propertyName]) {
+      return -1;
+    }
+    if (a[propertyName] < b[propertyName]) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return sortedArr;
+};
+
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+  const sortedResult = sortArrayOfObjectDescending(result, "year");
+  
+  // Rubah code ini dengan array hasil sorting secara descending
+  return sortedResult;
+}
